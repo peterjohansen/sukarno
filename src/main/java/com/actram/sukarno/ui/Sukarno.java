b2dec42f413f9a20fff4bd8bab8c08a37addc669
Peter André Johansen <peterandrejohansen@gmail.com>
2015-12-13 02:26:04 +0100

    Added existing files

---
+++ src/main/java/com/actram/sukarno/ui/Sukarno.java
@@ -0,0 +1,20 @@
+package com.actram.sukarno.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ *
+ * @author Peter André Johansen
+ */
+public class Sukarno extends Application {
+	public static void main(String[] args) {
+		Application.launch(Sukarno.class, args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+}
